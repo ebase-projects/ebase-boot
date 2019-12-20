b531d6f1270c9ec251afee15fa4ecd943ad7524f
liudw <ldw4033@gmail.com>
2019-12-20 16:39:45 +0800

    add common

---
+++ ebase-boot-project/ebase-boot-common/src/main/java/me/dwliu/lab/common/node/TreeUtils.java
@@ -0,0 +1,133 @@
+package me.dwliu.lab.common.node;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 树形结构工具类，如：菜单、部门等
+ *
+ * @author eric
+ * @since 1.0.0
+ */
+public class TreeUtils {
+
+    /**
+     * 根据pid，构建树节点
+     */
+    public static <T extends TreeNode> List<T> build(List<T> treeNodes, Long pid) {
+        //pid不能为空
+//		AssertUtils.isNull(pid, "pid");
+
+        List<T> treeList = new ArrayList<>();
+        for (T treeNode : treeNodes) {
+            if (pid.equals(treeNode.getPid())) {
+                treeList.add(findChildren(treeNodes, treeNode));
+            }
+        }
+
+        return treeList;
+    }
+
+    /**
+     * 查找子节点
+     */
+    private static <T extends TreeNode> T findChildren(List<T> treeNodes, T rootNode) {
+        for (T treeNode : treeNodes) {
+            if (rootNode.getId().equals(treeNode.getPid())) {
+                rootNode.getChildren().add(findChildren(treeNodes, treeNode));
+            }
+        }
+        return rootNode;
+    }
+
+    /**
+     * 构建树节点
+     */
+    public static <T extends TreeNode> List<T> build(List<T> treeNodes) {
+        List<T> result = new ArrayList<>();
+
+        //list转map
+        Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
+        for (T treeNode : treeNodes) {
+            nodeMap.put(treeNode.getId(), treeNode);
+        }
+
+//        for (T node : nodeMap.values()) {
+//            T parent = nodeMap.get(node.getPid());
+//            if (parent != null && !(node.getId().equals(parent.getId()))) {
+//                parent.getChildren().add(node);
+//                continue;
+//            }
+//
+//            result.add(node);
+//        }
+
+        genNodeList(result, nodeMap);
+
+        return result;
+    }
+
+
+    /**
+     * 构建树节点
+     *
+     * @param treeNodes       全部树节点
+     * @param searchTreeNodes 搜索的树节点
+     * @param <T>
+     * @return
+     */
+    public static <T extends TreeNode> List<T> buildBySearch(List<T> treeNodes, List<T> searchTreeNodes) {
+        List<T> result = new ArrayList<>();
+
+        //list转map
+        Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
+        Map<Long, T> nodeMapNew = new LinkedHashMap<>(treeNodes.size());
+        for (T treeNode : treeNodes) {
+            nodeMap.put(treeNode.getId(), treeNode);
+        }
+
+        for (T searchTreeNode : searchTreeNodes) {
+            nodeMapNew.put(searchTreeNode.getId(), searchTreeNode);
+
+            getPidNode(nodeMap, nodeMapNew, searchTreeNode);
+
+        }
+
+
+        genNodeList(result, nodeMapNew);
+
+        return result;
+    }
+
+    private static <T extends TreeNode> void genNodeList(List<T> result, Map<Long, T> nodeMapNew) {
+        for (T node : nodeMapNew.values()) {
+            T parent = nodeMapNew.get(node.getPid());
+            if (parent != null && !(node.getId().equals(parent.getId()))) {
+                parent.getChildren().add(node);
+                continue;
+            }
+
+            result.add(node);
+        }
+    }
+
+    private static <T extends TreeNode> void getPidNode(Map<Long, T> nodeMap, Map<Long, T> nodeMapNew, T searchTreeNode) {
+        if (searchTreeNode.getPid() != null) {
+            T t = nodeMap.get(searchTreeNode.getPid());
+            if (t == null) {
+                return;
+            }
+            nodeMapNew.put(searchTreeNode.getPid(), t);
+//            if (t.getPid() != null || t.getPid() == 0) {
+//                T t1 = nodeMap.get(t.getPid());
+//                nodeMapNew.put(searchTreeNode.getPid(), t1);
+//            }
+
+            getPidNode(nodeMap, nodeMapNew, t);
+        }
+    }
+
+
+}
