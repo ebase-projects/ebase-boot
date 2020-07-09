fc1a8b1dfacbd463bf27ec2da69bd642b2c67492
liudw <ldw4033@gmail.com>
2020-07-09 15:25:23 +0800

    feat: 不进行认证的URL

--- ebase-boot-project/ebase-boot-core/ebase-boot-core-security/src/main/java/me/dwliu/framework/core/security/constant/SecurityCoreConstant.java
+++ ebase-boot-project/ebase-boot-core/ebase-boot-core-security/src/main/java/me/dwliu/framework/core/security/constant/SecurityCoreConstant.java
@@ -7,45 +7,59 @@ package me.dwliu.framework.core.security.constant;
  * @date 2019-07-08 14:07
  **/
 public interface SecurityCoreConstant {
-    /**
-     * 资源服务器默认bean名称
-     */
-    String RESOURCE_SERVER_CONFIGURER = "resourceServerConfigurerAdapter";
+	/**
+	 * 资源服务器默认bean名称
+	 */
+	String RESOURCE_SERVER_CONFIGURER = "resourceServerConfigurerAdapter";
 
-    /**
-     * 内部
-     */
-    String FROM_IN = "Y";
+	/**
+	 * 内部
+	 */
+	String FROM_IN = "Y";
 
-    /**
-     * 标志
-     */
-    String FROM = "from";
+	/**
+	 * 标志
+	 */
+	String FROM = "from";
 
-    /**
-     * 角色前缀
-     */
-    String ROLE = "ROLE_";
+	/**
+	 * 角色前缀
+	 */
+	String ROLE = "ROLE_";
 
 
+	/**
+	 * 用户ID字段
+	 */
+	String DETAILS_USER_ID = "user_id";
 
-    /**
-     * 用户ID字段
-     */
-    String DETAILS_USER_ID = "user_id";
+	/**
+	 * 用户名字段
+	 */
+	String DETAILS_USERNAME = "username";
 
-    /**
-     * 用户名字段
-     */
-    String DETAILS_USERNAME = "username";
+	/**
+	 * 用户部门字段
+	 */
+	String DETAILS_DEPT_ID = "dept_id";
 
-    /**
-     * 用户部门字段
-     */
-    String DETAILS_DEPT_ID = "dept_id";
+	/**
+	 * 租户ID 字段
+	 */
+	String DETAILS_TENANT_ID = "tenant_code";
 
-    /**
-     * 租户ID 字段
-     */
-    String DETAILS_TENANT_ID = "tenant_code";
+
+	/**
+	 * 不进行认证的URL
+	 */
+	String[] IGNORING_URLS = {
+		"/actuator/**",
+		"/doc.html",
+		"/swagger-ui.html",
+		"/v2/api-docs/**",
+		"/webjars/**",
+		"/service-worker.js",
+		"/favicon.ico",
+		"/swagger-resources/**"
+	};
 }
