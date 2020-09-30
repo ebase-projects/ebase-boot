a9ead45cbc10214ca8c1bbb6d78256ac8d4e3b8f
liudw <ldw4033@gmail.com>
2020-09-30 19:59:03 +0800

    feat: 时间格式化

--- ebase-boot-project/ebase-boot-core/ebase-boot-core-oss/src/main/java/me/dwliu/framework/core/oss/model/FileInfo.java
+++ ebase-boot-project/ebase-boot-core/ebase-boot-core-oss/src/main/java/me/dwliu/framework/core/oss/model/FileInfo.java
@@ -40,7 +40,7 @@ public class FileInfo {
     /**
      * 文件上传时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date uploadDate;
     /**
      * 文件contentType
