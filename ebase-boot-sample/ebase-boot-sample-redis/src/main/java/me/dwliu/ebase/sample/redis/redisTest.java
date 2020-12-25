11f4f67caa14171da0cacde51e9c5a98cb8c1780
liudw <ldw4033@gmail.com>
2020-12-25 11:45:23 +0800

    feat: 增加redis demo

---
+++ ebase-boot-sample/ebase-boot-sample-redis/src/main/java/me/dwliu/ebase/sample/redis/redisTest.java
@@ -0,0 +1,18 @@
+package me.dwliu.ebase.sample.redis;
+
+import me.dwliu.framework.plugin.redis.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class redisTest {
+    @Autowired
+    private RedisService redisService;
+
+    @PostConstruct
+    public void test() {
+        redisService.set("wwwwww", "sssssssssssssssss");
+    }
+}
