e819020677a38fd03a68f07f99fa8fffe19df1f1
liudw <ldw4033@gmail.com>
2020-12-25 11:38:37 +0800

    feat: 增加redis

---
+++ ebase-boot-sample/ebase-boot-sample-redis/src/main/java/me/dwliu/ebase/sample/redis/EbaseBootSampleRedisApplication.java
@@ -0,0 +1,13 @@
+package me.dwliu.ebase.sample.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbaseBootSampleRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbaseBootSampleRedisApplication.class, args);
+    }
+
+}
---
+++ ebase-boot-sample/ebase-boot-sample-redis/src/test/java/me/dwliu/ebase/sample/redis/EbaseBootSampleRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dwliu.ebase.sample.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbaseBootSampleRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
