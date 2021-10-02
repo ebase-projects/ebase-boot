fc2694b4ad0ff9a92ffc59f55cb4910dd0c9ab4c
liudw <ldw4033@163.com>
2021-10-03 00:24:19 +0800

    feat:是否将末端叶子节点为空的对象设置为null ,默认是children=[]

--- ebase-boot-project/ebase-boot-common/src/main/java/me/dwliu/framework/common/node/TreeUtils.java
+++ ebase-boot-project/ebase-boot-common/src/main/java/me/dwliu/framework/common/node/TreeUtils.java
@@ -13,47 +13,67 @@ import java.util.Map;
  */
 public class TreeUtils {
 
-    /**
-     * 根据pid，构建树节点
-     */
-    public static <T extends TreeNode> List<T> build(List<T> treeNodes, Long pid) {
-        //pid不能为空
+	/**
+	 * 根据pid，构建树节点
+	 *
+	 * @param treeNodes
+	 * @param pid
+	 * @param <T>
+	 * @return
+	 */
+	public static <T extends TreeNode> List<T> build(List<T> treeNodes, Long pid) {
+		//pid不能为空
 //		AssertUtils.isNull(pid, "pid");
 
-        List<T> treeList = new ArrayList<>();
-        for (T treeNode : treeNodes) {
-            if (pid.equals(treeNode.getPid())) {
-                treeList.add(findChildren(treeNodes, treeNode));
-            }
-        }
+		List<T> treeList = new ArrayList<>();
+		for (T treeNode : treeNodes) {
+			if (pid.equals(treeNode.getPid())) {
+				treeList.add(findChildren(treeNodes, treeNode));
+			}
+		}
 
-        return treeList;
-    }
+		return treeList;
+	}
 
-    /**
-     * 查找子节点
-     */
-    private static <T extends TreeNode> T findChildren(List<T> treeNodes, T rootNode) {
-        for (T treeNode : treeNodes) {
-            if (rootNode.getId().equals(treeNode.getPid())) {
-                rootNode.getChildren().add(findChildren(treeNodes, treeNode));
-            }
-        }
-        return rootNode;
-    }
+	/**
+	 * 查找子节点
+	 */
+	private static <T extends TreeNode> T findChildren(List<T> treeNodes, T rootNode) {
+		for (T treeNode : treeNodes) {
+			if (rootNode.getId().equals(treeNode.getPid())) {
+				rootNode.getChildren().add(findChildren(treeNodes, treeNode));
+			}
+		}
+		return rootNode;
+	}
 
-    /**
-     * 构建树节点
-     */
-    public static <T extends TreeNode> List<T> build(List<T> treeNodes) {
-        List<T> result = new ArrayList<>();
+	/**
+	 * 构建树节点
+	 *
+	 * @param treeNodes
+	 * @param <T>
+	 * @return
+	 */
+	public static <T extends TreeNode> List<T> build(List<T> treeNodes) {
+		return build(treeNodes, false);
+	}
 
-        //list转map
-        Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
-        for (T treeNode : treeNodes) {
-            nodeMap.put(treeNode.getId(), treeNode);
-        }
+	/**
+	 * 构建树节点
+	 *
+	 * @param treeNodes
+	 * @param childrenNull 是否将末端叶子节点为空的对象设置为null ,默认是children=[]
+	 * @param <T>
+	 * @return
+	 */
+	public static <T extends TreeNode> List<T> build(List<T> treeNodes, boolean childrenNull) {
 
+		List<T> result = new ArrayList<>();
+		//list转map
+		Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
+		for (T treeNode : treeNodes) {
+			nodeMap.put(treeNode.getId(), treeNode);
+		}
 //        for (T node : nodeMap.values()) {
 //            T parent = nodeMap.get(node.getPid());
 //            if (parent != null && !(node.getId().equals(parent.getId()))) {
@@ -63,71 +83,106 @@ public class TreeUtils {
 //
 //            result.add(node);
 //        }
+		genNodeList(result, nodeMap);
 
-        genNodeList(result, nodeMap);
+		if (childrenNull) {
+			treeChildrenSetNull(result);
+		}
+		return result;
+	}
 
-        return result;
-    }
-
-
-    /**
-     * 构建树节点
-     *
-     * @param treeNodes       全部树节点
-     * @param searchTreeNodes 搜索的树节点
-     * @param <T>
-     * @return
-     */
-    public static <T extends TreeNode> List<T> buildBySearch(List<T> treeNodes, List<T> searchTreeNodes) {
-        List<T> result = new ArrayList<>();
-
-        //list转map
-        Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
-        Map<Long, T> nodeMapNew = new LinkedHashMap<>(treeNodes.size());
-        for (T treeNode : treeNodes) {
-            nodeMap.put(treeNode.getId(), treeNode);
-        }
-
-        for (T searchTreeNode : searchTreeNodes) {
-            nodeMapNew.put(searchTreeNode.getId(), searchTreeNode);
-
-            getPidNode(nodeMap, nodeMapNew, searchTreeNode);
-
-        }
+	/**
+	 * 构建条件树节点
+	 *
+	 * @param treeNodes       全部树节点
+	 * @param searchTreeNodes 搜索的树节点
+	 * @param <T>
+	 * @return
+	 */
+	public static <T extends TreeNode> List<T> buildBySearch(List<T> treeNodes, List<T> searchTreeNodes) {
+		return buildBySearch(treeNodes, searchTreeNodes, false);
+	}
 
 
-        genNodeList(result, nodeMapNew);
+	/**
+	 * 构建条件树节点
+	 *
+	 * @param treeNodes       全部树节点
+	 * @param searchTreeNodes 搜索的树节点
+	 * @param childrenNull    是否将末端叶子节点为空的对象设置为null ,默认是children=[]
+	 * @param <T>
+	 * @return
+	 */
+	public static <T extends TreeNode> List<T> buildBySearch(List<T> treeNodes,
+	                                                         List<T> searchTreeNodes,
+	                                                         boolean childrenNull) {
+		List<T> result = new ArrayList<>();
 
-        return result;
-    }
+		//list转map
+		Map<Long, T> nodeMap = new LinkedHashMap<>(treeNodes.size());
+		Map<Long, T> nodeMapNew = new LinkedHashMap<>(treeNodes.size());
+		for (T treeNode : treeNodes) {
+			nodeMap.put(treeNode.getId(), treeNode);
+		}
 
-    private static <T extends TreeNode> void genNodeList(List<T> result, Map<Long, T> nodeMapNew) {
-        for (T node : nodeMapNew.values()) {
-            T parent = nodeMapNew.get(node.getPid());
-            if (parent != null && !(node.getId().equals(parent.getId()))) {
-                parent.getChildren().add(node);
-                continue;
-            }
+		for (T searchTreeNode : searchTreeNodes) {
+			nodeMapNew.put(searchTreeNode.getId(), searchTreeNode);
+			getPidNode(nodeMap, nodeMapNew, searchTreeNode);
+		}
 
-            result.add(node);
-        }
-    }
+		genNodeList(result, nodeMapNew);
 
-    private static <T extends TreeNode> void getPidNode(Map<Long, T> nodeMap, Map<Long, T> nodeMapNew, T searchTreeNode) {
-        if (searchTreeNode.getPid() != null) {
-            T t = nodeMap.get(searchTreeNode.getPid());
-            if (t == null) {
-                return;
-            }
-            nodeMapNew.put(searchTreeNode.getPid(), t);
+		if (childrenNull) {
+			treeChildrenSetNull(result);
+		}
+
+		return result;
+	}
+
+	private static <T extends TreeNode> void genNodeList(List<T> result, Map<Long, T> nodeMapNew) {
+		for (T node : nodeMapNew.values()) {
+			T parent = nodeMapNew.get(node.getPid());
+			if (parent != null && !(node.getId().equals(parent.getId()))) {
+				parent.getChildren().add(node);
+				continue;
+			}
+
+			result.add(node);
+		}
+	}
+
+	private static <T extends TreeNode> void getPidNode(Map<Long, T> nodeMap, Map<Long, T> nodeMapNew, T searchTreeNode) {
+		if (searchTreeNode.getPid() != null) {
+			T t = nodeMap.get(searchTreeNode.getPid());
+			if (t == null) {
+				return;
+			}
+			nodeMapNew.put(searchTreeNode.getPid(), t);
 //            if (t.getPid() != null || t.getPid() == 0) {
 //                T t1 = nodeMap.get(t.getPid());
 //                nodeMapNew.put(searchTreeNode.getPid(), t1);
 //            }
 
-            getPidNode(nodeMap, nodeMapNew, t);
-        }
-    }
+			getPidNode(nodeMap, nodeMapNew, t);
+		}
+	}
+
+
+	/**
+	 * 表格树形数据展现时当children为空数组设置为null
+	 *
+	 * @param list 树形集合
+	 */
+	private static <T extends TreeNode> void treeChildrenSetNull(List<T> list) {
+		for (T p : list) {
+			if (p.getChildren() != null && p.getChildren().isEmpty()) {
+				p.setChildren(null);
+				continue;
+			}
+
+			treeChildrenSetNull(p.getChildren());
+		}
+	}
 
 
 }
