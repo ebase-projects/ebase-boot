4cca4fdae45d83a3079b8e0f3e6293b9e1b4c914
liudw <ldw4033@gmail.com>
2021-11-22 15:50:39 +0800

    枚举类型更改

--- ebase-boot-project/ebase-boot-core/ebase-boot-core-log/src/main/java/me/dwliu/framework/core/log/enums/LoginOperationEnum.java
+++ ebase-boot-project/ebase-boot-core/ebase-boot-core-log/src/main/java/me/dwliu/framework/core/log/enums/LoginOperationEnum.java
@@ -1,5 +1,10 @@
 package me.dwliu.framework.core.log.enums;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 登录操作枚举
  *
@@ -40,4 +45,82 @@ public enum LoginOperationEnum {
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
+
+
+	public static LoginOperationEnum getEnum(int value) {
+		LoginOperationEnum resultEnum = null;
+		LoginOperationEnum[] enumAry = LoginOperationEnum.values();
+		for (int i = 0; i < enumAry.length; i++) {
+			if (enumAry[i].getValue() == value) {
+				resultEnum = enumAry[i];
+				break;
+			}
+		}
+		return resultEnum;
+	}
+
+	public static Map<String, Map<String, Object>> toMap() {
+		LoginOperationEnum[] ary = LoginOperationEnum.values();
+		Map<String, Map<String, Object>> enumMap = new HashMap<String, Map<String, Object>>();
+		for (int num = 0; num < ary.length; num++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			String key = String.valueOf(getEnum(ary[num].getValue()));
+			map.put("value", String.valueOf(ary[num].getValue()));
+			map.put("desc", ary[num].getDesc());
+			enumMap.put(key, map);
+		}
+		return enumMap;
+	}
+
+	/**
+	 * 获取枚举和描述
+	 *
+	 * @return
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static List toList4Enum() {
+		LoginOperationEnum[] ary = LoginOperationEnum.values();
+		List list = new ArrayList();
+		for (int num = 0; num < ary.length; num++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("value", LoginOperationEnum.getEnum(ary[num].getValue()).name());
+			map.put("desc", ary[num].getDesc());
+			list.add(map);
+		}
+		return list;
+	}
+
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static List toList() {
+		LoginOperationEnum[] ary = LoginOperationEnum.values();
+		List list = new ArrayList();
+		for (int i = 0; i < ary.length; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("value", String.valueOf(ary[i].getValue()));
+			map.put("desc", ary[i].getDesc());
+			list.add(map);
+		}
+		return list;
+	}
+
+	/**
+	 * 取枚举的json字符串
+	 *
+	 * @return
+	 */
+	public static String getJsonStr() {
+		LoginOperationEnum[] enums = LoginOperationEnum.values();
+		StringBuffer jsonStr = new StringBuffer("[");
+		for (LoginOperationEnum senum : enums) {
+			if (!"[".equals(jsonStr.toString())) {
+				jsonStr.append(",");
+			}
+			jsonStr.append("{id:'").append(senum).append("',desc:'").append(senum.getDesc()).append("',value:'")
+				.append(senum.getValue()).append("'}");
+		}
+		jsonStr.append("]");
+		return jsonStr.toString();
+	}
+
 }
--- ebase-boot-project/ebase-boot-core/ebase-boot-core-log/src/main/java/me/dwliu/framework/core/log/enums/OperationStatusEnum.java
+++ ebase-boot-project/ebase-boot-core/ebase-boot-core-log/src/main/java/me/dwliu/framework/core/log/enums/OperationStatusEnum.java
@@ -1,5 +1,10 @@
 package me.dwliu.framework.core.log.enums;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 操作状态枚举
  *
@@ -40,4 +45,82 @@ public enum OperationStatusEnum {
 	public void setDesc(String desc) {
 		this.desc = desc;
 	}
+
+
+	public static OperationStatusEnum getEnum(int value) {
+		OperationStatusEnum resultEnum = null;
+		OperationStatusEnum[] enumAry = OperationStatusEnum.values();
+		for (int i = 0; i < enumAry.length; i++) {
+			if (enumAry[i].getValue() == value) {
+				resultEnum = enumAry[i];
+				break;
+			}
+		}
+		return resultEnum;
+	}
+
+	public static Map<String, Map<String, Object>> toMap() {
+		OperationStatusEnum[] ary = OperationStatusEnum.values();
+		Map<String, Map<String, Object>> enumMap = new HashMap<String, Map<String, Object>>();
+		for (int num = 0; num < ary.length; num++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			String key = String.valueOf(getEnum(ary[num].getValue()));
+			map.put("value", String.valueOf(ary[num].getValue()));
+			map.put("desc", ary[num].getDesc());
+			enumMap.put(key, map);
+		}
+		return enumMap;
+	}
+
+	/**
+	 * 获取枚举和描述
+	 *
+	 * @return
+	 */
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static List toList4Enum() {
+		OperationStatusEnum[] ary = OperationStatusEnum.values();
+		List list = new ArrayList();
+		for (int num = 0; num < ary.length; num++) {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("value", OperationStatusEnum.getEnum(ary[num].getValue()).name());
+			map.put("desc", ary[num].getDesc());
+			list.add(map);
+		}
+		return list;
+	}
+
+
+	@SuppressWarnings({"rawtypes", "unchecked"})
+	public static List toList() {
+		OperationStatusEnum[] ary = OperationStatusEnum.values();
+		List list = new ArrayList();
+		for (int i = 0; i < ary.length; i++) {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("value", String.valueOf(ary[i].getValue()));
+			map.put("desc", ary[i].getDesc());
+			list.add(map);
+		}
+		return list;
+	}
+
+	/**
+	 * 取枚举的json字符串
+	 *
+	 * @return
+	 */
+	public static String getJsonStr() {
+		OperationStatusEnum[] enums = OperationStatusEnum.values();
+		StringBuffer jsonStr = new StringBuffer("[");
+		for (OperationStatusEnum senum : enums) {
+			if (!"[".equals(jsonStr.toString())) {
+				jsonStr.append(",");
+			}
+			jsonStr.append("{id:'").append(senum).append("',desc:'").append(senum.getDesc()).append("',value:'")
+				.append(senum.getValue()).append("'}");
+		}
+		jsonStr.append("]");
+		return jsonStr.toString();
+	}
+
 }
