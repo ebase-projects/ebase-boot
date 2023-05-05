e14d429e73b957791d3bd1f1e47ab68fdef9f4d4
liudw <ldw4033@gmail.com>
2023-05-05 19:42:32 +0800

    feat: 优化 CustomJsonAuthenticationEntryPoint.java

--- ebase-boot-project/ebase-boot-integrations/ebase-boot-integration-security/src/main/java/me/dwliu/framework/integration/security/handler/CustomJsonAuthenticationEntryPoint.java
+++ ebase-boot-project/ebase-boot-integrations/ebase-boot-integration-security/src/main/java/me/dwliu/framework/integration/security/handler/CustomJsonAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import cn.hutool.http.HttpStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import me.dwliu.framework.common.code.SystemResultCode;
@@ -14,6 +15,7 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 /**
  * 匿名未登录的时候访问,需要登录的资源的调用类
@@ -29,17 +31,19 @@ public class CustomJsonAuthenticationEntryPoint implements AuthenticationEntryPo
 	@SneakyThrows
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 						 AuthenticationException authException) {
+		log.error("没有权限不允许访问:{}", authException.getMessage());
 		ObjectMapper objectMapper = new ObjectMapper();
 		response.setCharacterEncoding("UTF-8");
 		// response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
 		response.setContentType("application/json;charset=UTF-8");
 		Result<String> result = new Result<>();
-		result.setCode(SecurityResultCode.LOGIN_ERROR_CODE);
-		if (authException != null) {
-			result.setMsg(authException.getMessage());
-			result.setData(authException.getMessage());
-		}
-		response.setStatus(HttpStatus.HTTP_UNAUTHORIZED);
+
+		result.setCode(SecurityResultCode.FORBIDDEN_CODE);
+		result.setMsg("没有权限不允许访问");
+		result.setTimestamp(LocalDateTime.now());
+		result.setData(authException.getMessage());
+
+		response.setStatus(HttpStatus.HTTP_FORBIDDEN);
 		response.getWriter().append(objectMapper.writeValueAsString(result));
 	}
 }
